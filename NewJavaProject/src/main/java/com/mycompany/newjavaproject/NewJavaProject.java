cdf103ffc23b6b6adc5f3608e7198077ece47307
emilefp@hotmail.com <94909858+MiloLaD@users.noreply.github.com>
2025-03-05 18:11:57 +0100

    test

---
+++ NewJavaProject/src/main/java/com/mycompany/newjavaproject/NewJavaProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.newjavaproject;
+
+/**
+ *
+ * @author milolad
+ */
+public class NewJavaProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
